99e39a2c758e464e85aa29dfd942fb45f3d00d64
Abdelkabiir <abdelkabir.watil@gmail.com>
2020-02-08 14:56:28 +0100

    experiment on auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,29 @@
+import {
+    Body,
+    Controller,
+    Post,
+    Res,
+    HttpStatus,
+  } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('session')
+  export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post()
+    async create(@Res() res, @Body('session') loginData) {
+      const loginOp = this.authService.createSession(loginData);
+      if (loginOp) {
+        return res.status(HttpStatus.CREATED).json({
+          message: 'session created',
+          loginOp,
+        });
+      } else {
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+          message: 'error creating session',
+          loginOp,
+        });
+      }
+    }
+  }
