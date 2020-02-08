647a1369165fe47a67c760a2995ffef462172fc4
Abdelkabiir <abdelkabir.watil@gmail.com>
2020-02-08 14:56:06 +0100

    Add password module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PasswordModule } from './password/password.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PasswordModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
